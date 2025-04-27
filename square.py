317750547fc96459d43a15e59ed06c370cba6db3
Tasfia45 <arjutanvir31@gmail.com>
2025-04-27 22:45:34 +0600

    Add files via upload

---
+++ square.py
@@ -0,0 +1,3 @@
+a=float(input("enter a length:"))
+area=a**2
+print(f"Thre area of square is: {area:0.2f}")
---
+++ triangle.py
@@ -0,0 +1,4 @@
+b=float(input("Enter a base of triangle:"))
+h=float(input("Enter a height of  triangle:"))
+area=0.5*h*b
+print(f"The area of triangle is:{area:o.2f}")
